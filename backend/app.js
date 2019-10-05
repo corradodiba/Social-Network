70f0f5021422c7f3ed946fef62882e59165aed09
Corrado Dibattista <corradodiba@gmail.com>
2019-10-05 20:10:02 +0200

    Re-organizing backend routes

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ const express = require("express");
 const bodyParse = require("body-parser");
 const mongoose = require("mongoose");
 
-const Post = require("./models/post");
+const postsRouter = require("./router/posts");
+
 
 mongoose.connect("mongodb+srv://corrado:yhhGomWiaUGUNX9I@cluster0-f6xao.mongodb.net/social-network?retryWrites=true&w=majority")
   .then(() => {
@@ -29,63 +30,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content
-  });
-  post.save();
-  console.log(post);
-  res.status(201).json({
-    message: "Post added successfully!"
-  })
-})
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents
-    });
-  });
-});
-
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.body.id,
-    title: req.body.title,
-    content: req.body.content
-  });
-  Post.updateOne({_id: req.params.id}, post)
-  .then(result => {
-    res.status(200).json({
-      message: 'Post updated successfully!'
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if(post){
-      res.status(200).json({
-        message: "Post found!",
-        post
-      });
-    } else {
-      res.status(404).json({
-        message: "Post not found!"
-      });
-    }
-  });
-})
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id })
-    .then(result => {
-      res.status(200).json({ message: "Post deleted!" });
-    });
-});
-
+app.use("/api/posts", postsRouter);
 
 module.exports = app;
